af93c3c9a49ed82dea8fbca67a8a82e98be8797e
GabeGramblicka <gabegramblicka@gmail.com>
2026-01-13 00:47:20 -0700

    working facts!

---
+++ data.js
@@ -0,0 +1,20 @@
+const data = {
+  "posts": [
+    {
+      "fact": "Computers can only read in 0s and 1s, called binary.",
+      "programmer": "\t\"The cost of adding a feature isn't just the time it takes to code it. The cost also includes the addition of an obstacle to future expansion. The trick is to pick the features that don't fight each other.\"\n\t- John Carmack, Creator of DOOM, Quake"
+    },
+    {
+      "fact": "Your phone is over a million times more powerful than the computers that helped land astronauts on the moon!",
+      "programmer": "\t\"He's a guy that just wants to help, and he's a little bit too helpful sometimes - and there's something fun and vulnerable about that.\"\n\t- Kevan Atteberry, Creator of Clippy"
+    },
+    {
+      "fact": "The \"@\" symbol in email addresses was chosen by the inventor of email because it meant \"at\" and wasn't used for anything else on keyboards at the time.",
+      "programmer": "\t\"The only phrase I've ever disliked is, 'Why, we've always done it that way,'\"\n\t- Grace Hopper, Who found the first computer bug"
+    },
+    {
+      "fact": "The average person blinks 15-20 times per minute, but when using a computer, they only blink 7 times per minute! That's why your eyes can feel dry after screen time.",
+      "programmer": "\t\"The only phrase I've ever disliked is, 'Why, we've always done it that way,'\"\n\t- Grace Hopper, Who found the first computer bug"
+    },
+  ]
+};
